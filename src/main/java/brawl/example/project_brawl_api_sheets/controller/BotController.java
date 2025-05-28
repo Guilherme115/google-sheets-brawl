842629ef99ba4a391f0adac5a90ed7cdf78b3dfc
Guilherme Ribeiro <Guilhermesantos.2008@outlook.com.br>
2025-05-27 22:36:00 -0300

    Added a debug line

--- src/main/java/brawl/example/project_brawl_api_sheets/controller/BotController.java
+++ src/main/java/brawl/example/project_brawl_api_sheets/controller/BotController.java
@@ -1,17 +1,19 @@
 package brawl.example.project_brawl_api_sheets.controller;
 
-import brawl.example.project_brawl_api_sheets.config.BotDiscordAPIConfig;
 import brawl.example.project_brawl_api_sheets.service.ActionsBotService;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BotController extends ListenerAdapter {
-    private ActionsBotService service;
 
+    private final ActionsBotService service;
+
+    public BotController(ActionsBotService service) {
+        this.service = service;
+    }
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
@@ -20,20 +22,15 @@ public class BotController extends ListenerAdapter {
         event.reply(resposta).queue();
     }
 
-        @Override
-        public void onMessageReceived(MessageReceivedEvent event) {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+
         String userID = event.getAuthor().getId();
         String msg = event.getMessage().getContentRaw();
-        if (event.getAuthor().isBot()) return;
-        String resposta = service.mainFlow(userID,msg);
+        String resposta = service.mainFlow(userID, msg);
         event.getChannel().sendMessage(resposta).queue();
+        System.out.println("MENSAGEM RECEBIDA: '" + msg + "' (tamanho: " + msg.length() + ")");
 
     }
-
-
-
-
-
-
-
 }
