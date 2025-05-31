0abc531fce97e8014823e53a513e4a30a5aa76f6
Guilherme Ribeiro <Guilhermesantos.2008@outlook.com.br>
2025-05-31 11:54:30 -0300

    (Added) Model to guide individual player information

--- src/main/java/brawl/example/project_brawl_api_sheets/model/PlayerValidModel.java
+++ src/main/java/brawl/example/project_brawl_api_sheets/model/PlayerValidModel.java
@@ -1,4 +1,12 @@
 package brawl.example.project_brawl_api_sheets.model;
 
+import lombok.*;
+
+@Data
 public class PlayerValidModel {
+
+    @NonNull
+    private String name;
+    private Integer trophies;
+    private boolean isQualifiedFromChampionshipChallenge;
 }
