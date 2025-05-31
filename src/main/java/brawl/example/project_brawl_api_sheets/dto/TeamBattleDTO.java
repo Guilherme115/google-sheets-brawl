5dd0ecf40afe7e3abfed80db533f1b93989f1383
Guilherme Ribeiro <Guilhermesantos.2008@outlook.com.br>
2025-05-31 19:36:45 -0300

    feat: add TeamBattleDTO class

--- src/main/java/brawl/example/project_brawl_api_sheets/dto/TeamBattleDTO.java
+++ src/main/java/brawl/example/project_brawl_api_sheets/dto/TeamBattleDTO.java
@@ -1,4 +1,15 @@
 package brawl.example.project_brawl_api_sheets.dto;
 
+import brawl.example.project_brawl_api_sheets.model.BrawlRequestMODEL;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@Data
+@AllArgsConstructor
 public class TeamBattleDTO {
+    private String teamName;
+    private List<BrawlRequestMODEL.BattleLogInfo> battles;
 }
