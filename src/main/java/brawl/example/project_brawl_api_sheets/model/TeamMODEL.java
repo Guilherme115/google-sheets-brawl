5a49ecf055f89f7fadbe299182759718064755e7
Guilherme Ribeiro <Guilhermesantos.2008@outlook.com.br>
2025-06-01 17:56:13 -0300

    (added) model made to receive database tags and names

--- src/main/java/brawl/example/project_brawl_api_sheets/model/TeamMODEL.java
+++ src/main/java/brawl/example/project_brawl_api_sheets/model/TeamMODEL.java
@@ -1,4 +1,13 @@
 package brawl.example.project_brawl_api_sheets.model;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.*;
+
+@Data
+@Component
 public class TeamMODEL {
+    private String teamName;
+    private List<String> playersTags;
 }
