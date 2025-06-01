e272ced30209a34d92cb1ae70d5220716ac9d117
Guilherme Ribeiro <Guilhermesantos.2008@outlook.com.br>
2025-06-01 18:09:10 -0300

    feat: added more query methods to MongoDB entity for better data access

--- src/main/java/brawl/example/project_brawl_api_sheets/entity/PlayerTagData.java
+++ src/main/java/brawl/example/project_brawl_api_sheets/entity/PlayerTagData.java
@@ -1,11 +1,17 @@
 package brawl.example.project_brawl_api_sheets.entity;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import brawl.example.project_brawl_api_sheets.model.TeamPlayerTag;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface PlayerTagData extends MongoRepository<PlayerTagEntity, String> {
     boolean existsByTags(String tags);
-
+    boolean existsByTeamName(String teamName);
+    List<PlayerTagEntity> findByTeamName(String teamName);
+    List<PlayerTagEntity> findByDiscordID(String discordID);
+    List<PlayerTagEntity> findByTagsContaining(String tag);
+    List<TeamPlayerTag> findAllBy();
 }
+
