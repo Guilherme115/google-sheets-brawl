1e06feeef08fd1e4447f5a24b562eb2d4b51a034
Guilherme Ribeiro <Guilhermesantos.2008@outlook.com.br>
2025-06-01 18:10:16 -0300

    feat: added interface to simplify queries on the entity

---
+++ src/main/java/brawl/example/project_brawl_api_sheets/model/TeamPlayerTag.java
@@ -0,0 +1,12 @@
+package brawl.example.project_brawl_api_sheets.model;
+
+import brawl.example.project_brawl_api_sheets.entity.PlayerTagEntity;
+
+import java.util.List;
+
+public interface TeamPlayerTag {
+
+    String getTeamName();
+    List<String> getTags();
+
+}
