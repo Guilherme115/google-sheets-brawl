d6400d67fe6c1ba247c008f40302efe45e854586
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-15 00:52:13 -0300

    fix : uptade project structure cleanaly

---
+++ brawl-core/src/main/java/brawl/example/project_brawl_api_sheets/ProjectBrawlApiSheetsApplication.java
@@ -0,0 +1,15 @@
+package brawl.example.project_brawl_api_sheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ProjectBrawlApiSheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBrawlApiSheetsApplication.class, args);
+	}
+
+}
---
+++ brawl-core/src/main/java/brawl/example/project_brawl_api_sheets/integration_sheets/model/TeamPlayerTag.java
@@ -0,0 +1,10 @@
+package brawl.example.project_brawl_api_sheets.integration_sheets.model;
+
+import java.util.List;
+
+public interface TeamPlayerTag {
+
+    String getTeamName();
+    List<String> getTags();
+
+}
