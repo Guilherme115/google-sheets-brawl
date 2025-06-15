5655f8b7dafc3521672cea1ba89c0bc36669a6a6
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-15 14:28:49 -0300

    feat : added token that will return to the front to keep the section

--- brawl-web/src/main/java/com/example/web_manager/auth/controller/AuthController.java
+++ brawl-web/src/main/java/com/example/web_manager/auth/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.example.web_manager.auth.controller;
 
+import com.example.web_manager.auth.dto.DiscordUserResponse;
+import com.example.web_manager.auth.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
