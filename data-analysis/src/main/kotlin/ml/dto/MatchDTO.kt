2582a194d43254e627ab89d7fa4075fed040d809
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-19 19:40:22 -0300

    feat : Created the DTO to remove the information from the Google Sheets API

---
+++ data-analysis/src/main/kotlin/ml/dto/MatchDTO.kt
@@ -0,0 +1,22 @@
+package com.guilherme.ml.dto
+
+data class MatchDTO (
+
+    val teamName: String,
+    val battleTime: String,
+    val mode: String,
+    val result: String,
+    val duration: Int,
+    val player1: String,
+    val player2: String,
+    val player3: String,
+    val brawlerP1: String,
+    val brawlerP2: String,
+    val brawlerP3: String,
+    val opponent1: String,
+    val opponent2: String,
+    val opponent3: String,
+    val opponentBrawler1: String,
+    val opponentBrawler2: String,
+    val opponentBrawler3: String,
+)
