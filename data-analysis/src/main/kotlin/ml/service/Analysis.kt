9ebb8bc1280ab87ec85b5c0788e93ff6bbedbc0c
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-19 19:43:13 -0300

    feat : Some 'analyzes' to test some functions in Kotlin, I'm still learning to use.

---
+++ data-analysis/src/main/kotlin/ml/service/Analysis.kt
@@ -0,0 +1,47 @@
+package com.guilherme.ml.service
+
+import com.guilherme.ml.dto.MatchDTO
+import org.springframework.stereotype.Service
+import kotlin.collections.listOf
+
+@Service
+class Analysis {
+    fun mostUsedBrawlers(matches: List<MatchDTO>,topN: Int = 5): List<Pair<String, Int>> {
+    val allBrawlers = matches.flatMap { match ->
+        listOf(
+            match.brawlerP1,
+            match.brawlerP2,
+            match.brawlerP3,
+            match.opponentBrawler1,
+            match.opponentBrawler2,
+            match.opponentBrawler3,
+        )
+    }
+        val contagem = allBrawlers.groupingBy { it }.eachCount()
+
+        return contagem.toList().sortedByDescending { (_, value) -> value }.take(topN)
+
+    }
+
+    fun teamWithMostVictory(matches: List<MatchDTO>, topN: Int = 5): List<Pair<String, Int>> {
+        return matches
+            .filter { it.result.equals("victory", ignoreCase = true)  }
+            .groupingBy { it.teamName.uppercase() }
+            .eachCount()
+            .toList()
+            .sortedByDescending { (_, value) -> value }
+            .take(topN)
+    }
+
+
+    fun winRateByTeam(matches: List<MatchDTO>): List<Pair<String, Double>> {
+        return matches
+            .groupBy { it.teamName.uppercase() }
+            .map { (team, partidasDoTime) ->
+                val total = partidasDoTime.size
+                val vitorias = partidasDoTime.count { it.result.equals("victory", ignoreCase = true) }
+                team to (vitorias.toDouble() / total)
+            }
+            .sortedByDescending { it.second }
+    }
+}
