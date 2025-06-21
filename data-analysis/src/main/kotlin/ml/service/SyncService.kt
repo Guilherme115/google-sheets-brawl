bd82252b8db030aaac517ce150db7cdafc6ab551
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-21 15:52:16 -0300

    feat: implement SyncService methods for data conversion and analysis

--- data-analysis/src/main/kotlin/ml/service/SyncService.kt
+++ data-analysis/src/main/kotlin/ml/service/SyncService.kt
@@ -1,7 +1,31 @@
 package com.guilherme.ml.service
 
-import org.springframework.stereotype.Service
+import com.guilherme.ml.dto.MatchDTO
 
-@Service
-class SyncService  {
+class SyncService (
+    private val convert : ConvertService,
+    private val analysis: Analysis,
+    private val sheetssService: SheetssService) {
+
+    fun config () : List <MatchDTO> {
+        val lists = sheetssService.getData()
+        return convert.convertToDto(lists)
+
+    }
+    fun mostUsedBrawlers () : List<Pair<String, Int>> {
+        val list = config()
+        return analysis.mostUsedBrawlers(list)
+
+    }
+    fun teamWithMostVictory () {
+        val list = config()
+        analysis.teamWithMostVictory(list)
+    }
+
+      fun winRateGlobal () {
+        val list = config()
+        analysis.winRateByTeam(list)
+    }
+
+
 }
