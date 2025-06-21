5bcca9d6f36ffbc9fc4756a31e7bef331540652a
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-21 15:53:18 -0300

    refactor: update AnalysisController to use SyncService for analysis

--- data-analysis/src/main/kotlin/ml/controller/AnalysisController.kt
+++ data-analysis/src/main/kotlin/ml/controller/AnalysisController.kt
@@ -1,17 +1,22 @@
 package com.guilherme.ml.controller
 
 import com.guilherme.ml.service.Analysis
+import com.guilherme.ml.service.SheetssService
+import com.guilherme.ml.service.SyncService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/analysis")
-class AnalysisController (private val analysis: Analysis) {
 
+class AnalysisController(
+    private val syncService: SyncService
+
+) {
 
     @GetMapping("/MostUsedBrawlers")
-    fun mostUsedBrawlers() : List<Pair<String, Int>> {
-analysis.mostUsedBrawlers()
+    fun mostUsedBrawlers(): List<Pair<String, Int>> {
+        return syncService.mostUsedBrawlers()
     }
 }
