8463b04c8f76fb97ef51d10c5797a4484b897a6b
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-23 21:26:56 -0300

    refactor: update SecurityConfig to adjust request matcher permissions for improved endpoint access control

--- brawl-web/src/main/java/com/example/web_manager/auth/config/SecurityConfig.java
+++ brawl-web/src/main/java/com/example/web_manager/auth/config/SecurityConfig.java
@@ -19,7 +19,8 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login/**").permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
