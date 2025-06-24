2b46b14dcee6895be24ea89adc65fbcb856fd47f
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-23 21:23:55 -0300

    refactor: update mostUsedBrawlers function to return BrawlerUsage model for improved clarity and usability

--- data-analysis/src/main/kotlin/ml/service/Analysis.kt
+++ data-analysis/src/main/kotlin/ml/service/Analysis.kt
@@ -1,28 +1,30 @@
 package com.guilherme.ml.service
 
 import com.guilherme.ml.dto.MatchDTO
+import com.guilherme.ml.model.BrawlerUsage
 import org.springframework.stereotype.Service
 import kotlin.collections.listOf
 
 @Service
 class Analysis {
-    fun mostUsedBrawlers(matches: List<MatchDTO>,topN: Int = 5): List<Pair<String, Int>> {
-    val allBrawlers = matches.flatMap { match ->
-        listOf(
-            match.brawlerP1,
-            match.brawlerP2,
-            match.brawlerP3,
-            match.opponentBrawler1,
-            match.opponentBrawler2,
-            match.opponentBrawler3,
-        )
-    }
+    fun mostUsedBrawlers(matches: List<MatchDTO>, topN: Int = 5): List<BrawlerUsage> {
+        val allBrawlers = matches.flatMap { match ->
+            listOf(
+                match.brawlerP1,
+                match.brawlerP2,
+                match.brawlerP3,
+                match.opponentBrawler1,
+                match.opponentBrawler2,
+                match.opponentBrawler3,
+            )
+        }
         val contagem = allBrawlers.groupingBy { it }.eachCount()
 
-        return contagem.toList().sortedByDescending { (_, value) -> value }.take(topN)
-
+        return contagem
+            .map { (brawler, count) -> BrawlerUsage( brawlerName = brawler, usageCount = count) }
+            .sortedByDescending { it.usageCount }
+            .take(topN)
     }
-
     fun teamWithMostVictory(matches: List<MatchDTO>, topN: Int = 5): List<Pair<String, Int>> {
         return matches
             .filter { it.result.equals("victory", ignoreCase = true)  }
@@ -44,4 +46,5 @@ class Analysis {
             }
             .sortedByDescending { it.second }
     }
-}
+    }
+
