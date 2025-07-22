bf472b8e81b902618a275f49a10116296793c5d1
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-07-22 17:53:09 -0300

    refactor: move BattleMatchRepository to the correct package and annotate with @Repository for consistency
    - Add `existsByBattleTime` method to optimize battle existence checks.
    - Update BattleMatch entity with proper annotations, table mapping, and relationship definitions.

--- brawl-core/src/main/java/brawl/example/project_brawl_api_sheets/integration_sheets/brawl_sheets/model/BattleMatch.java
+++ brawl-core/src/main/java/brawl/example/project_brawl_api_sheets/integration_sheets/brawl_sheets/model/BattleMatch.java
@@ -1,16 +1,20 @@
 package brawl.example.project_brawl_api_sheets.integration_sheets.brawl_sheets.model;
 
-
 import jakarta.persistence.*;
-import lombok.Data;
-
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
 import java.util.List;
-@Data
+
+@Getter
+@Setter
+@Entity
+@Table(name = "battle_match")
 public class BattleMatch {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long internalId; //
+        private Long internalId;
 
         private String battleTime;
         private String mode;
@@ -18,7 +22,12 @@ public class BattleMatch {
         private String result;
         private int duration;
 
-        @OneToMany(mappedBy = "battleMatch", cascade = CascadeType.ALL, orphanRemoval = true)
-        private List<TeamMODEL> teams;
-    }
-
+        // UMA Batalha tem MUITOS Times
+        @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+        @JoinTable(
+                name = "battle_teams", // Nome da tabela de junção
+                joinColumns = @JoinColumn(name = "battle_match_id"), // Coluna que referencia BattleMatch
+                inverseJoinColumns = @JoinColumn(name = "team_model_id") // Coluna que referencia TeamMODEL
+        )
+        private List<TeamMODEL> teams = new ArrayList<>();
+}
