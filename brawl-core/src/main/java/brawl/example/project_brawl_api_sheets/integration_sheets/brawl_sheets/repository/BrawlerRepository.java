a31b255e2d1f7d9dc4331f49b5bd919ae748b828
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-07-22 17:52:14 -0300

    refactor: move BrawlerRepository to the correct package and annotate with @Repository for clarity and consistency

--- brawl-core/src/main/java/brawl/example/project_brawl_api_sheets/integration_sheets/brawl_sheets/repository/BrawlerRepository.java
+++ brawl-core/src/main/java/brawl/example/project_brawl_api_sheets/integration_sheets/brawl_sheets/repository/BrawlerRepository.java
@@ -1,7 +1,9 @@
-package brawl.example.project_brawl_api_sheets.integration_sheets.brawl_sheets.entity;
+package brawl.example.project_brawl_api_sheets.integration_sheets.brawl_sheets.repository;
 
 import brawl.example.project_brawl_api_sheets.integration_sheets.brawl_sheets.model.BrawlerMODEL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BrawlerRepository extends JpaRepository<BrawlerMODEL,String> {
 }
